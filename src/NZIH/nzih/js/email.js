7cbfbc2ec01b3cf66f2cf9a77d3cd04e05b8a541
hanjia2014 <hanj@datacom.co.nz>
2015-11-06 14:02:24 +1300

    email

---
+++ src/NZIH/nzih/js/email.js
@@ -0,0 +1,31 @@
+﻿function send() {
+    var key = 'YMP7ayh71LTbsqqzaxwDDQ';
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var subject = $("#subject").val();
+    var message = $("#message").val();
+
+    $.ajax({
+        type: "POST",
+        url: "https://mandrillapp.com/api/1.0/messages/send.json",
+        data: {
+            'key': key,
+            'message': {
+                'from_email': email,
+                'to': [
+                  {
+                      'email': 'info@nzih.co.nz',
+                      'name': name,
+                      'type': 'to'
+                  }
+                ],
+                'subject': subject,
+                'html': message
+            }
+        }
+    }).success(function () {
+
+    }).error(function (err) {
+        alert(err);
+    });
+}
---
+++ src/NZIH/nzih/js/map.js
@@ -0,0 +1,71 @@
+﻿$("#map-canvas").gMap({
+
+    styles: [{
+        stylers: [
+    {
+        featureType: 'water', // set the water color
+        elementType: 'geometry.fill', // apply the color only to the fill
+        stylers: [
+            { color: '#adc9b8' }
+        ]
+    }, {
+        featureType: 'landscape.natural', // set the natural landscape
+        elementType: 'all',
+        stylers: [
+            { hue: '#809f80' },
+            { lightness: -35 }
+        ]
+    }
+    , {
+        featureType: 'poi', // set the point of interest
+        elementType: 'geometry',
+        stylers: [
+            { hue: '#f9e0b7' },
+            { lightness: 30 }
+        ]
+    }, {
+        featureType: 'road', // set the road
+        elementType: 'geometry',
+        stylers: [
+            { hue: '#d5c18c' },
+            { lightness: 14 }
+        ]
+    }, {
+        featureType: 'road.local', // set the local road
+        elementType: 'all',
+        stylers: [
+            { hue: '#ffd7a6' },
+            { saturation: 100 },
+            { lightness: -12 }
+        ]
+    }
+        ]
+    }],
+    zoomControl: true,
+    scrollwheel: false,
+    maptype: 'ROADMAP',
+    markers: [
+        {
+            latitude: -41.1691284,
+            longitude: 174.8243729,
+            icon: {
+                image: "images/location.png",
+                iconsize: [85, 121],
+                iconanchor: [85, 121]
+            }
+        },
+
+    ],
+    icon: {
+        image: "images/location.png",
+        iconsize: [85, 121],
+        iconanchor: [85, 121]
+    },
+    latitude: -41.1691284,
+    longitude: 174.8243729,
+
+    zoom: 15,
+    mapTypeId: 'Styled'
+
+
+});
